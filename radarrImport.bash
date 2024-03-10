5fe5617aea47c25f971d7158adc596fd23b96915
trickiegt <trickiegt@gmail.com>
2024-03-09 19:15:13 -0500

    Update radarrImport.bash
    renamed repo / change path to reflect

--- radarrImport.bash
+++ radarrImport.bash
@@ -6,4 +6,4 @@ if [ $radarr_movie_id == $null ]; then
   #This way the powershell script will not present an interactive menu
   radarr_movie_id="0"
 fi
-/opt/microsoft/powershell/7/pwsh /home/Trailarr_/Trailers.ps1 $radarr_movie_id #Update to reflect where you saved the scripts or powershell location
+/opt/microsoft/powershell/7/pwsh /home/Trailarr/Trailers.ps1 $radarr_movie_id #Update to reflect where you saved the scripts or powershell location
